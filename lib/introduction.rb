049e296ef05c81ffcab042b3b3756771c8c03ede
noella callahan <calla147@gmail.com>
2020-06-29 15:48:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,5 @@ puts  "Hi, my name is #{josh}."
 end
 
 def introduction_with_language(name, language)
-puts  "Hi, my name is #{name} and I am learning to program in puts #{language}."
+puts  "Hi, my name is #{name} and I am learning to program in #{language}."
 end
